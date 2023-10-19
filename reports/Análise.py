5a7292d5bfbf7fd116306a5618fa3729eccedb20
Eduardo Gomes <edu_merckx@yahoo.com.br>
2023-10-19 12:06:13 -0300

    update base structure for report

--- reports/Análise.py
+++ reports/Análise.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-import src.generate_graphs as generate_graphs
-import src.get_data as get_data
 
 st.write("""
     # Tech Challenge #03 - Grupo 27 
@@ -10,19 +7,11 @@ st.write("""
 """)
          
 st.info("""
-    
+    Nesse trabalho temos o objetivo de mostrar o processo de obtenção, ingestão e análise de dados do 
+    [PNAD-COVID-19](https://www.ibge.gov.br/estatisticas/downloads-estatisticas.html?caminho=Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_PNAD_COVID19/Microdados/Dados) 
+    do IBGE
 """)
-        
 
-tab_grafico_historico, tab_seasonal, tab_adf, tab_acf, tab_models = st.tabs(['Gráfico Histórico', 'Decompondo sazonalidade', 'Teste ADFuller', 'Autocorrelação - ACF/PACF', 'Modelos - Teste'])
 
-with tab_grafico_historico:
-    df = get_data._get_all_indicators_data()
-    st.plotly_chart(
-        generate_graphs._grafico_historico(df),
-        use_container_width=True,
-    )
+tab_dados, tab_tecnologia, tab_transformacao, tab_ingestao, tab_modelo = st.tabs(['Dados utilizados', 'Tecnologia', 'Transformação', 'Ingestão', 'Modelo'])
 
-    st.markdown(f"""
-        
-    """)
