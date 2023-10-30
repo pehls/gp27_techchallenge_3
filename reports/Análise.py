c6fc3be4e58787acc1304b9df091b47749b381d1
Gabriel Pehls <gabrielpehls@hotmail.com>
2023-10-30 00:25:53 -0300

    ajuste modelo dados

--- reports/Análise.py
+++ reports/Análise.py
@@ -67,24 +67,6 @@ Como último ponto, foi criada a camada de dados confiáveis (`trusted`), conten
     
 with tab_modelo_dados:
     st.markdown("""
-        Utilizando um modelo de árvores chamado `Random Forest`, que cria várias árvores de decisão, de maneira aleatória, gerando uma espécie de votação para realizar decisões sobre a classe a qual o dado pertence, 
-        iremos observar a importância das features, utilizando o método de permutação.
-        Tal método visa capturar a influência que cada variável tem nas predições do modelo, ao alterar aleatoriamente os valores de cada coluna, uma coluna por vez.
+       
     """)
-    st.plotly_chart(
-        generate_graphs._plot_modelo_completo(get_data._get_importance_data_allfeatures()),
-        use_container_width=True,
-    )
-
-    st.markdown("""
-        Utilizando a função `seasonal_decompose` não foi identificado nenhum padrão sazonal.
-        Foi utilizado o valor 5 no parâmetro _period_ por ser esse o ciclo de dias da bolsa
-    """)
-    st.plotly_chart(
-        generate_graphs._plot_modelo_completo(get_data._get_importance_data_sintomas()),
-        use_container_width=True,
-    )
-
-    st.markdown("""
-        A partir dessa análise inicial, vamos destrinchar os detalhes de cada categoria de variável, nas próximas páginas :)
-    """)
+    
